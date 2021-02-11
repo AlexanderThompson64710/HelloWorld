05a472b3941a4794ff3f4a161dac9ee176371cfe
Alexander Thompson <tuo15862@temple.edu>
2021-02-10 21:37:42 -0500

    This program adds a button that changes the text field to You have clicked the button when the button is pressed.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,34 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        View.OnClickListener onClickListener = new myButtonClickListener();
+
+        button.setOnClickListener(onClickListener);
     }
+
+    class myButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            textView.setText("You have clicked the button");
+        }
+    }
+
 }
